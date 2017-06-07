42a680ab0437f90c2addbbfe504eab2b4476d1ab
Zamzam Nurzaman <znurzamanz@gmail.com>
2017-06-07 20:22:56 +0700

    benerin route

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,7 +3,51 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
                         // controller/metode
 //PENELITIAN
+$route['penelitian/beranda'] = 'penelitian/beranda';
+-  $route['penelitian/beranda2'] = 'penelitian/beranda2';
+-  $route['penelitian/lihat'] = 'penelitian/view';
+-  $route['penelitian/tambah'] = 'penelitian/tambah';
+-  $route['penelitian/update/(:any)'] = 'penelitian/update/$1';
+-  $route['penelitian/edit_sekertaris'] = 'penelitian/edit_sekertaris';
+-  //laporan
+-  $route['penelitian/laporan'] = 'penelitian/laporan';
+-  $route['penelitian/cetak_laporan'] = 'penelitian/cetak_laporan';
+-  //cetak
+-  $route['penelitian/cetak/(:any)'] = 'penelitian/cetak/$1';
+-  //Email
+-  $route['penelitian/mail'] = 'penelitian/mail';
+-  $route['penelitian/send_mail/(:any)'] = 'penelitian/send_mail/$1';
+-  //Pengaturan
+-  $route['penelitian/pengaturan'] = 'penelitian/pengaturan';
+-  $route['penelitian/tambah_bagian'] = 'penelitian/tambah_bagian';
+-  $route['penelitian/edit_bagian/(:any)'] = 'penelitian/edit_bagian/$1';
+-  //penempatan
+-  $route['penempatan/form_penempatan/(:any)'] = 'penempatan/form_penempatan/$1';
+-  $route['penempatan/edit_penempatan/(:any)'] = 'penempatan/edit_penempatan/$1';
+-  $route['penempatan/penempatan'] = 'penempatan/penempatan';
+-  $route['penempatan/set_penempatan'] = 'penempatan/set_penempatan';
 
+-//PKL
+-  $route['pkl/lihat'] = 'pkl/view';
+-  $route['pkl/laporan'] = 'pkl/laporan';
+-  //penempatan
+-  $route['pkl/form_penempatan/(:any)'] = 'pkl/form_penempatan/$1';
+-  $route['pkl/edit_penempatan/(:any)'] = 'pkl/edit_penempatan/$1';
+-  $route['pkl/penempatan'] = 'pkl/penempatan';
+-  $route['pkl/set_penempatan'] = 'pkl/set_penempatan';
+-
+-  //Email
+-  $route['pkl/mail'] = 'pkl/mail';
+-  $route['pkl/send_mail/(:any)'] = 'pkl/send_mail/$1';
+-
+-//ADMIN
+-  $route['admin/lihat'] = 'admin/view';
+-  $route['admin/tambah'] = 'admin/tambah';
+-  $route['admin/update/(:any)'] = 'admin/update/$1';
+-
+-//ANDROID ACCESS
+-  $route['penelitian/android_data_penelitian'] = 'android/penelitian';
+-  $route['penelitian/android_data_pkl'] = 'android/pkl';
 
 //ON PROGRESS
 $route['penelitian/backup'] = 'penelitian/backup';
